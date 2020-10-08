4a71590d096b13fb9f4896194914cc2e47bb651d
ykliba <mediaboxing2@gmail.com>
2020-10-08 22:13:40 +0900

    vue

---
+++ axios.min.js
@@ -0,0 +1,15 @@
+new Vue({
+  el: '#app',
+  data: {
+    // あらかじめ空リストを用意しておく
+    list: []
+  },
+  created: function () {
+    axios.get('list.json').then(function (response) {
+      // 取得完了したらlistリストに代入
+      this.list = response.data
+    }.bind(this)).catch(function (e) {
+      console.error(e)
+    })
+  }
+})
